ac4c6285cdb8717278bd6d4cc6acce4b94808437
irdickinson <irdickinson@crimson.ua.edu>
2023-03-23 15:03:30 -0500

    Added User and CustomUserManager in models.py

--- sales/app/models.py
+++ sales/app/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+
 
 
 # the model being used for a basic table on the website
@@ -29,3 +31,32 @@ class Document(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects=CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+        
