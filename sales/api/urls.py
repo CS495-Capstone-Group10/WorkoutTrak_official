17923ca98511173fe621995b321393e1430f577f
Ben D <98532754+bderl@users.noreply.github.com>
2023-04-03 19:01:31 -0500

    CRUD groups

--- sales/api/urls.py
+++ sales/api/urls.py
@@ -4,7 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('user-list/', views.userList, name="user-list"),
+    path('group-list/', views.groupList, name="group-list"),
+    path('workout-list/', views.workoutList, name="workout-list"),
+    
     path('user-detail/<str:pk>/', views.userDetail, name="user-detail"),
+    path('group-detail/<str:pk>/', views.groupDetail, name="group-detail"),
+    path('workout-detail/<str:pk>/', views.workoutDetail, name="workout-detail"),
+
+    path('create-group/', views.createGroup, name="create-group"),
+    path('update-group/<str:pk>/', views.updateGroup, name="update-group"),
+    path('delete-group/<str:pk>/', views.deleteGroup, name="delete-group"),
+    
     
 ]
 # added this static line above
--- sales/api/views.py
+++ sales/api/views.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view # For function based view decorater
 from rest_framework.response import Response
-from .serializers import CustomUserSerializer
-from app.models import CustomUser, Group
+from .serializers import CustomUserSerializer, GroupSerializer, WorkoutSerializer
+from app.models import CustomUser, Group, Workout
 # Create your views here.
 
 @api_view(['GET'])
@@ -25,13 +25,57 @@ def userList(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+def groupList(request):
+    group = Group.objects.all()
+    serializer = GroupSerializer(group, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def workoutList(request):
+    workout = Workout.objects.all()
+    serializer = WorkoutSerializer(workout, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 def userDetail(request,pk):
     user = CustomUser.objects.get(username=pk)
     serializer = CustomUserSerializer(user, many=False)
     return Response(serializer.data)
 
-@api_view(['POST'])
-def createGroup(request,pk):
-    user = Group.objects.get()
-    serializer = CustomUserSerializer(user, many=False)
+@api_view(['GET'])
+def groupDetail(request,pk):
+    group = Group.objects.get(id=pk)
+    serializer = GroupSerializer(group, many=False)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def workoutDetail(request,pk):
+    workout = Workout.objects.all(id=pk)
+    serializer = WorkoutSerializer(workout, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createGroup(request):
+    serializer = GroupSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateGroup(request,pk):
+    group = Group.objects.get(id=pk)
+    serializer = GroupSerializer(instance=group, data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteGroup(request,pk):
+    group = Group.objects.get(id=pk)
+    group.delete()
+    return Response("Item successfully deleted")
