02d3904d44810378bbd4e5df3593911e398e02f9
Asit07 <amsingh@crimson.ua.edu>
2023-04-10 09:58:14 -0500

    added functionality to filter data

--- sales/Data/data_analytics.py
+++ sales/Data/data_analytics.py
@@ -1,5 +1,8 @@
+import numpy as np 
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches  
+import seaborn as sns
 # Reading in Files
 file_2022_fall = pd.ExcelFile('2022_fall_workouts.xlsx')
 file_2023_spring = pd.ExcelFile('2023_spring_workouts.xlsx')
@@ -22,8 +25,8 @@ dfs_list_2022_fall = list(dfs_2022_fall.values())
 dfs_list_2023_spring = list(dfs_2023_spring.values())
 
 # Saving as a concatenated list
-df_all_2022_fall = pd.concat(dfs_list_2022_fall, ignore_index=True)
-df_all_2023_spring = pd.concat(dfs_list_2023_spring, ignore_index=True)
+df_all_2022_fall = pd.concat(dfs_list_2022_fall)
+df_all_2023_spring = pd.concat(dfs_list_2023_spring)
 
 # Name to be found and made a file of
 name = 'Markel'
@@ -35,3 +38,31 @@ output_filename = f'./{name}_output.xlsx'
 with pd.ExcelWriter(output_filename) as writer:
     df_filtered_2022_fall.to_excel(writer, index=False, sheet_name='Filtered Data')
     df_filtered_2023_spring.to_excel(writer, index=False, sheet_name='Filtered Data')
+
+# File to do analytics
+df_analytic_file = pd.read_excel(output_filename)
+
+# Info about the file
+print(df_analytic_file.info())
+print(df_analytic_file.head())
+print(df_analytic_file.describe())
+
+desired_data = ['PR', '4x10', '6x6.40', '5x8', '4x10', '3x13.20', '6k']
+
+df_desired_data = {}
+for data in desired_data:
+    df_desired_data[data] = df_analytic_file.filter(like=data)
+
+desired_data_list = pd.concat(list(df_desired_data.values()))
+
+print(df_desired_data)
+filtered_filename = f'./{name}_filtered.xlsx'
+# generating output file
+with pd.ExcelWriter(filtered_filename) as writer:
+    desired_data_list.to_excel(writer, index=False, sheet_name='Filtered Data')
+
+# max_val = {}
+# for data in desired_data:
+#     max_val[data] = max(df_desired_data[data])
+    
+# print(max_val)
