d4d942d17ee38cadc28331eaae03dfcd61951b3a
Isaac Dickinson <isaacrdickinson@gmail.com>
2023-04-19 16:17:09 -0500

    Testing of upDocX_page.js Temporary changes being made Work in Progress

--- sales/app/static/js/upDocX_page.js
+++ sales/app/static/js/upDocX_page.js
@@ -12,10 +12,10 @@ function UploadDocument() {
     const [description, setDescription] = useState('');
     const [file, setFile] = userState(null);
 
-    const logout = async (e)=>{
-        await localStorage.setItem("salesToken",null);
-        window.location = "/login";
-      };
+    // const logout = async (e)=>{
+    //     await localStorage.setItem("salesToken",null);
+    //     window.location = "/login";
+    //   };
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
@@ -40,67 +40,29 @@ function UploadDocument() {
         formData.append('file', file);
 
         // sends data to django view in upDocuments.py
-        await axios.post('/api/upload_document/', formData); //************************************ */
-    }
+        //await axios.post('/api/upload_document/', formData); //************************************ */
+    };
 
     return ( 
         // website upload page details 
         <div>
-        <nav className="navbar">
-          <div className="navbar__container">
-            <a href="/homeX" id="navbar__logo">
-              <i className="fas fa-gem"></i>WorkoutTrak : Social
-            </a>
-            <div className="navbar__toggle" id="mobile-menu">
-              <span className="bar"></span>
-              <span className="bar"></span>
-              <span className="bar"></span>
-            </div>
-            <ul className="navbar__menu">
-              <li className="navbar__item">
-                <a href="/homeX" className="navbar__links">Home</a>
-              </li>
-              <li className="navbar__item">
-                <a href="/workoutsX" className="navbar__links">Workouts</a>
-              </li>
-              <li className="navbar__item">
-                <a href="/athletesX" className="navbar__links">Athletes</a>
-              </li>
-              <li className="navbar__item">
-                <a href="/recordsX" className="navbar__links">Records</a>
-              </li>
-              <li className="navbar__item">
-                <a href="/socialX" className="navbar__links">Social</a>
-              </li>
-              <li className="navbar__item">
-                <a href="/profileX" className="navbar__links">Profile</a>
-              </li>
-              <li className="navbar__item">
-                <a href="/loginX" className="navbar__links" onClick={logout}>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </nav> 
-  
-   
-         
-          <div className="services">
-          <h1>-</h1>
-           <div className="services__container">
-            
-             <div className="services__card1">
-               <h2>Form a Group</h2>
-               <button className="main__content"><a href="/socialX">Create Group</a></button>
-             </div>
-             <div className="services__card1">
-               <h2>Join a Group</h2>
-               <button className="main__content"><a href="/socialX">Join Group</a></button>
-             </div>
-           </div>
-         </div> 
-  
-         <script type="text/jsx" src="/static/js/app2.js"></script> 
-      </div>
+            <h1> Upload Document</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="description"></label>
+                    <textarea placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+                </div>
+                <div>
+                    <label htmlFor="file">File:</label>
+                    <input type = "file" onChange = {(e) => setFile(e.target.files[0])} />
+                </div>
+                <button type="submit">Upload</button>
+            </form>
+        </div>
     );
 }
 
