235572bba3eb3d49438f5d2ef6de72dd02900be2
Isaac Dickinson <isaacrdickinson@gmail.com>
2023-04-22 17:41:06 -0500

    Add profile2X to views and urls

--- sales/app/views.py
+++ sales/app/views.py
@@ -133,6 +133,10 @@ def profileX_view(request):
     context = {}
     return render(request, "profileX.html", context=context)
 
+def profile2X_view(request):
+    context = {}
+    return render(request, "profile2X.html", context=context)
+
 def socialX_view(request):
     context = {}
     return render(request, "socialX.html", context=context)
--- sales/sales/urls.py
+++ sales/sales/urls.py
@@ -63,6 +63,7 @@ urlpatterns = [
     path('recordsX', views.recordsX_view), 
     path('createAccountX', views.createAccountX_view),
     path('upDocX', views.upDocX_view),
+    path('profile2X', views.profile2X_view),
     
     
     
